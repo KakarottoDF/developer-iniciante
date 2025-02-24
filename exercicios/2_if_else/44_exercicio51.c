815d11003064abd4054c7e046c2368e21a772b23
Cristiabo Ayu PC Cidadania4U <cristiano.ayub@cidadania4u.com.br>
2025-02-24 15:25:31 -0300

    Inclusão do exercício 51

---
+++ exercicios/2_if_else/44_exercicio51.c
@@ -0,0 +1,15 @@
+/*
+Faça um algoritmo que leia dois números e mostre qual o maior dos dois.
+
+SINTESE
+    ENTRADA: num1, num2
+    PROCESSAMENTO: comparação
+    SAIDA: maior número
+
+*/
+
+#include <stdio.h>
+
+int main(){
+
+}
