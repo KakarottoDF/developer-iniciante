c0e1d9a03a5f35d08cc4a496a96f9e7fa57e9cee
Cristiabo Ayu PC Cidadania4U <cristiano.ayub@cidadania4u.com.br>
2025-02-26 15:09:39 -0300

    Atualizando exercícios

--- exercicios/4_for/exercicio-92.c
+++ exercicios/4_for/exercicio-92.c
@@ -1,12 +1,26 @@
 /*
 SINTESE
-    ENTRADA: 
-    PROCESSAMENTO: 
-    SAIDA: 
+    ENTRADA: numero
+    PROCESSAMENTO: valorDeA += (numero + ((numero - (i) / i +1)))
+    SAIDA: valorDeA
 */
 
 #include <stdio.h>
 
 int main(){
+
+    int numero;
+    float valorDeA = 0;
+
+    printf("Digite um numero inteiro positivo: ");
+    scanf("%d", &numero);
+
+    valorDeA = numero;
+
+    for(int i = 2; i <= numero; i++){
+        valorDeA += (float)(numero - (i - 1)) / i;
+    }
+
+    printf("\nO valor de A = %.2f", valorDeA);
     
 }
--- exercicios/4_for/exercicio-93.c
+++ exercicios/4_for/exercicio-93.c
@@ -1,6 +1,6 @@
 /*
 SINTESE
-    ENTRADA: 
+    ENTRADA: o for irá fazer a entrada
     PROCESSAMENTO: 
     SAIDA: 
 */
@@ -8,5 +8,15 @@ SINTESE
 #include <stdio.h>
 
 int main(){
+
+    double grao = 1;
+    double total = 1;
+
+    for (int i = 2; i <= 64; i++) {
+        grao *= 2;
+        total += grao;
+    }
+
+    printf("O total de graos e: %.0f\n", total);
     
 }
