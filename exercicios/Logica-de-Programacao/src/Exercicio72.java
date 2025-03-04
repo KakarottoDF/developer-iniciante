c24b95c6309940c73cc465b87d2bae2b22ae308b
Cristiano Ayub <cristiano.ayub@gmail.com>
2025-03-04 16:19:14 -0300

    Comitando só o Exercicio72

--- exercicios/Logica-de-Programacao/src/Exercicio72.java
+++ exercicios/Logica-de-Programacao/src/Exercicio72.java
@@ -3,6 +3,22 @@ SINTESE
     ENTRADA: quantidadeDeRepeticoesDoSistema, entradaDeNumero
     PROCESSAMENTO: maior
     SAIDA: maior
+    Um determinado clube de futebol pretende classificar seus atletas em categorias
+e para isto ele contratou um programador para criar um programa que
+executasse esta tarefa. Para isso o clube criou uma tabela que continha a faixa
+etária do atleta e sua categoria. A tabela está demonstrada abaixo:
+IDADE
+CATEGORIA
+De 05 a 10
+Infantil
+De 11 a 15
+Juvenil
+De 16 a 20
+Junior
+De 21 a 25
+Profissional
+Construa um programa que solicite o nome e a idade de um atleta e imprima a
+sua categoria.
 */
 
 public class Exercicio72 {
