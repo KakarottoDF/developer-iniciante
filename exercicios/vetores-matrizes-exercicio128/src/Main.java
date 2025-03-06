c5a294dc4a45ea0e9a039c6ff73334a4c655ed5c
Cristiano Ayub <cristiano.ayub@gmail.com>
2025-03-06 17:32:06 -0300

    Exercício 128

---
+++ exercicios/vetores-matrizes-exercicio128/src/Main.java
@@ -0,0 +1,31 @@
+/*
+SINTESE
+    ENTRADA: primeiroVetor,segundoVetor
+    PROCESSAMENTO: criar um terceiro vetor com o dobro do tamanho dos outros dois
+    SAIDA: terceiroVetor
+Faça um algoritmo que faça a união de dois vetores de mesmo tamanho e
+mesmo tipo em um terceiro vetor com dobro do tamanho.
+*/
+
+public class Main {
+    public static void main(String[] args) {
+        int []primeiroVetor = new int[4];
+        int []segundoVetor = new int[primeiroVetor.length];
+        int []terceiroVetor = new int[(primeiroVetor.length + segundoVetor.length)];
+
+        System.out.println("Primeiro Vetor:");
+        for(int i = 0; i < primeiroVetor.length; i++){
+            System.out.println(i + 1);
+        }
+
+        System.out.println("Segundo Vetor:");
+        for(int i = 0; i < segundoVetor.length; i++){
+            System.out.println(i + 1);
+        }
+
+        System.out.println("Terceiro Vetor:");
+        for(int i = 0; i < terceiroVetor.length; i++){
+            System.out.println(i + 1);
+        }
+    }
+}
