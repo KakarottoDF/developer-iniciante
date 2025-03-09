b3b3de08dfaee83ab943acb06520f386a42355c9
Cristiano Ayub <cristiano.ayub@gmail.com>
2025-03-09 15:29:08 -0300

    Subindo novo projeto da lista nova Exercício01

---
+++ exercicios-novos/Exercicio01/untitled/src/Main.java
@@ -0,0 +1,14 @@
+/*
+ O Java possui uma importante característica, que é ser independente de plataforma.
+Explique, com suas palavras, o que é essa característica e como o Java fornece essa
+característica. Crie um projeto java e escreva o texto em forma de comentário. Sim, é só
+isso.
+
+
+
+ */
+public class Main{
+    public static void main(String[] args) {
+
+    }
+}
