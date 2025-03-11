8c9b46433692cb6bdd925206db0f35945e673b33
Cristiano Ayub <cristiano.ayub@gmail.com>
2025-03-11 18:05:35 -0300

    Atualizando Exercícios. TESTE

--- exercicios-novos/Exercicio09/src/Main.java
+++ exercicios-novos/Exercicio09/src/Main.java
@@ -7,7 +7,7 @@ SINTESE
 Uma escola deseja fazer um cadastro dos seus alunos. Um aluno possui nome, matrícula e
 situação, que pode ser regular ou bolsista. O usuário informará quantos alunos existem em uma turma. Você receberá os dados de todos os alunos e, na sequência, você deve
 apresentar todos os alunos informados, a quantidade de alunos regulares que existe e a
-quantidade de alunos bolsistas. a
+quantidade de alunos bolsistas.
 */
 
 public class Main {
