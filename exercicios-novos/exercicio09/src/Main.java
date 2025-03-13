6fcd5e44f0da2675e1966078dbad774bb86c78a6
Cristiano Ayub <cristiano.ayub@gmail.com>
2025-03-13 11:28:00 -0300

    Exercicio09 Finalizado

--- exercicios-novos/exercicio09/src/Main.java
+++ exercicios-novos/exercicio09/src/Main.java
@@ -1,15 +1,3 @@
-/*
-SINTESE
-    ENTRADA: nomeDoAluno, matriculaDoAluno, situacaoDoAluno, quantidadeDeAlunosNaSala
-    PROCESSAMENTO: quantidadeDeAlunosRegulares, quantidadeDeAlunosBolsistas 
-    SAIDA: nomeDoAluno, matriculaDoAluno, situacaoDoAluno, quantidadeDeAlunosRegulares, quantidadeDeAlunosBolsistas
-
-Uma escola deseja fazer um cadastro dos seus alunos. Um aluno possui nome, matrícula e
-situação, que pode ser regular ou bolsista. O usuário informará quantos alunos existem em uma turma. Você receberá os dados de todos os alunos e, na sequência, você deve
-apresentar todos os alunos informados, a quantidade de alunos regulares que existe e a
-quantidade de alunos bolsistas.
-*/
-
 public class Main {
     public static void main(String[] args) {
         Turma turma = new Turma();
