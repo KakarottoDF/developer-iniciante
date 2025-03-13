3114285e754bdb5397df35ad4159fdbed968f10a
Cristiano Ayub <cristiano.ayub@gmail.com>
2025-03-13 17:56:51 -0300

    Exercicio09 Reestruturado

--- exercicios-novos/exercicio09/src/Leitor.java
+++ exercicios-novos/exercicio09/src/Leitor.java
@@ -19,25 +19,6 @@ public class Leitor{
         return lerInt();
     }
 
-    public static boolean lerSituacao(String msg){
-        System.out.print(msg);
-        return lerSituacao();
-    }
-
-    public static boolean lerSituacao(){
-        String resposta;
-        while(true) {
-            resposta = new Scanner(System.in).nextLine().trim().toUpperCase();
-            if (resposta.equals("S")) {
-                return true;
-            } else if (resposta.equals("N")) {
-                return false;
-            } else {
-                System.out.print("Entrada inválida. Por favor, digite 'S' para Sim ou 'N' para Não: ");
-            }
-        }
-    }
-
     public static Turma lerTurma(int totalDeAlunos) {
         Turma turma = new Turma();
         turma.quantidadeDeAlunoEmTurma = totalDeAlunos;
@@ -61,6 +42,25 @@ public class Leitor{
         aluno.situacaoBolsista = lerSituacao("O aluno é Bolsista? ");
 
         return aluno;
+    }
+
+    public static boolean lerSituacao(String msg){
+        System.out.print(msg);
+        return lerSituacao();
+    }
+
+    public static boolean lerSituacao(){
+        String resposta;
+        while(true) {
+            resposta = new Scanner(System.in).nextLine().trim().toUpperCase();
+            if (resposta.equals("S")) {
+                return true;
+            } else if (resposta.equals("N")) {
+                return false;
+            } else {
+                System.out.print("Entrada inválida. Por favor, digite 'S' para Sim ou 'N' para Não: ");
+            }
+        }
     }
 
 }
