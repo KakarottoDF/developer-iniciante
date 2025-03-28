f2a42ed509e4c795e9875131d589a7c179586c91
Cristiano Ayub <cristiano.ayub@gmail.com>
2025-03-28 16:31:35 -0300

    Exercicio05

--- nova-lista-marco-2025/exercicio05/src/Reader.java
+++ nova-lista-marco-2025/exercicio05/src/Reader.java
@@ -29,35 +29,41 @@ public class Reader{
         return lerDouble();
     }
 
-    public GerenciarAtleta preencherAtletas(GerenciarAtleta gerenciarAtletas) {
-        int quantidadeDeAtletas = 3;
+    private void preencherInscricao(Atleta novoAtleta) {
         boolean inscricaoValida = false;
+
+        while (!inscricaoValida) {
+            int inscricao = lerInt("Digite o número de inscrição do atleta: ");
+            novoAtleta.setInscricao(inscricao);
+            if (novoAtleta.getInscricao() != 0) {
+                inscricaoValida = true;
+            }
+        }
+    }
+
+    public void preencherAtletas(GerenciarAtleta gerenciarAtletas) {
+        int quantidadeDeAtletas = 3;
 
         gerenciarAtletas.atletas = new Atleta[quantidadeDeAtletas];
 
         for (int i = 0; i < quantidadeDeAtletas; i++) {
             Atleta novoAtleta = new Atleta(gerenciarAtletas);
-            inscricaoValida = false;
 
-            while (!inscricaoValida) {
-                int inscricao = lerInt("Digite o número de inscrição do atleta: ");
-                novoAtleta.setInscricao(inscricao);
-                if (novoAtleta.getInscricao() != 0) {
-                    inscricaoValida = true;
-                }
-            }
+            preencherInscricao(novoAtleta);
 
             novoAtleta.setNome(lerString("Digite o nome do atleta: "));
             novoAtleta.setIdade(lerInt("Digite a idade do atleta: "));
             novoAtleta.setTempoDeProva(lerDouble("Digite o tempo de prova do atleta: "));
 
-            for (int j = 0; j < gerenciarAtletas.atletas.length; j++) {
-                if (gerenciarAtletas.atletas[j] == null) {
-                    gerenciarAtletas.atletas[j] = novoAtleta;
-                }
+            adicionarAtleta(gerenciarAtletas, novoAtleta);
+        }
+    }
+
+    private void adicionarAtleta(GerenciarAtleta gerenciarAtletas, Atleta novoAtleta) {
+        for (int i = 0; i < gerenciarAtletas.atletas.length; i++) {
+            if (gerenciarAtletas.atletas[i] == null) {
+                gerenciarAtletas.atletas[i] = novoAtleta;
             }
         }
-
-        return gerenciarAtletas;
     }
 }
