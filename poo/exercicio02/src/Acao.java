6642b08ff088438cdf189fa6197fd44d34a154e9
Cristiano Ayub <cristiano.ayub@gmail.com>
2025-04-09 17:32:15 -0300

    Exercício02

--- poo/exercicio02/src/Acao.java
+++ poo/exercicio02/src/Acao.java
@@ -9,7 +9,7 @@ public class Acao {
 
     public int getTrimestre(){
 
-        return switch (this.mes) {
+        return switch (getMes()) {
             case 1, 2, 3 -> 1;
             case 4, 5, 6 -> 2;
             case 7, 8, 9 -> 3;
