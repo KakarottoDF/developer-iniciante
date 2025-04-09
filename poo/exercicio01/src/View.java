20936e5ef390d72a2e7c895cd10b06788940257b
Cristiano Ayub <cristiano.ayub@gmail.com>
2025-04-09 17:00:51 -0300

    Exercício02

--- poo/exercicio01/src/View.java
+++ poo/exercicio01/src/View.java
@@ -10,13 +10,12 @@ public class View{
         return franquia;
     }
 
-    public static Anatel cadastrar(Anatel anatel){
+    public static void cadastrar(Anatel anatel){
         boolean continuar = true;
 
         while(continuar){
             anatel.adicionar(cadastrarFranquia());
             continuar = Reader.lerBoolean("Deseja continuar? [S]Sim [N]Não: ", "Digite somente [S]Sim [N]Não", "S", "N");
         }
-        return anatel;
     }
 }
