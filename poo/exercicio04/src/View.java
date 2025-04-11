f6e358088ab2cd802702f5a63ed06ec412319fb1
Cristiano Ayub <cristiano.ayub@gmail.com>
2025-04-11 15:09:43 -0300

    Exercício04

--- poo/exercicio04/src/View.java
+++ poo/exercicio04/src/View.java
@@ -9,6 +9,7 @@ public class View {
         placas.setQuantidadeDePinosDigitais(Reader.lerInt("Informe a quantidade de pinos digitais: "));
         placas.setSinalMLP(Reader.lerBoolean("Essa placa tem sinal MLP? [S]Sim [N]Não: ", "Digite somente [S]Sim ou [N]Não", "S", "N"));
         placas.setQuantidadeDeEntradasAnalogicas(Reader.lerInt("Informe a quantidade de entradas analógicas: "));
+        placas.setPrecoBase(Reader.lerDouble("Informe o preço base desta placa R$ "));
 
         return placas;
     }
