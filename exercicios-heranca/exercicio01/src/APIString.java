0f81e3957204fa2c4ed25cc7ef4579f9b5ca2022
Cristiano Ayub <cristiano.ayub@gmail.com>
2025-04-16 10:57:16 -0300

    Exercício01

--- exercicios-heranca/exercicio01/src/APIString.java
+++ exercicios-heranca/exercicio01/src/APIString.java
@@ -3,6 +3,10 @@ public class APIString {
         Usuario usuario = new Usuario(Reader.lerString("Digite uma frase: "));
 
         System.out.println(usuario.contarLetraA());
+        System.out.println(usuario.quantidadeDeCaracteres());
+        System.out.println(usuario.qualificacaoDaFrase());
+        System.out.println(usuario.getFrase().toUpperCase());
+        System.out.println(usuario.quantidadeDePalavras());
 
     }
 }
--- exercicios-heranca/exercicio01/src/Usuario.java
+++ exercicios-heranca/exercicio01/src/Usuario.java
@@ -30,4 +30,45 @@ public class Usuario{
 
         return contador;
     }
+
+    public int quantidadeDeCaracteres(){
+        String frase = getFrase();
+
+        if (frase == null || frase.isEmpty()) {
+            return 0;
+        }
+
+        return frase.length();
+    }
+
+    public String qualificacaoDaFrase(){
+        String resposta;
+
+        if(quantidadeDeCaracteres() < 10){
+            resposta = "PEQUENA";
+        }else{
+            if(quantidadeDeCaracteres() > 9 && quantidadeDeCaracteres() < 29){
+                resposta = "MEDIA";
+            }else{
+                resposta = "GRANDE";
+            }
+        }
+
+        return resposta;
+    }
+
+    public int quantidadeDePalavras(){
+
+        String frase;
+        frase = getFrase().trim().toUpperCase();
+
+        if(frase.isEmpty()){
+            return 0;
+        }
+
+        String[] palavras = frase.split("\\s+");
+        // Divide por espaços em branco e a diferença entre \\s+ e " " é que o segundo só quebra em espaços simples. Se tiver vários espaços seguidos (" "), ele considera como vários espaços vazios, e o array vai ter strings vazias no meio.
+
+        return palavras.length;
+    }
 }
