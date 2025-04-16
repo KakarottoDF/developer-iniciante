88e491732d9fbc64048436acb0b9389263086155
Cristiano Ayub <cristiano.ayub@gmail.com>
2025-04-16 11:08:55 -0300

    Exercício02

---
+++ exercicios-heranca/exercicio02/src/Administrativo.java
@@ -0,0 +1,11 @@
+public class Administrativo extends Funcionario{
+    private int dependentes;
+
+    public int getDependentes() {
+        return this.dependentes;
+    }
+
+    public void setDependentes(int dependentes) {
+        this.dependentes = dependentes;
+    }
+}
---
+++ exercicios-heranca/exercicio02/src/Professor.java
@@ -0,0 +1,11 @@
+public class Professor extends Funcionario{
+    private String titulacao;
+
+    public String getTitulacao() {
+        return this.titulacao;
+    }
+
+    public void setTitulacao(String titulacao) {
+        this.titulacao = titulacao;
+    }
+}
