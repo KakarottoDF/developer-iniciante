0f6856a13bd0e7d41a1f440cfe369e52c592d72b
Cristiano Ayub <cristiano.ayub@gmail.com>
2025-04-18 11:37:20 -0300

    Exercicio02

--- exercicios-heranca/exercicio02/src/View.java
+++ exercicios-heranca/exercicio02/src/View.java
@@ -1,4 +1,26 @@
 public class View{
+    private static Diretor cadastrarDiretor() {
+        Diretor diretor = new Diretor();
+
+        diretor.setNome(Reader.lerString("Informe o nome do Diretor: "));
+        diretor.setCpf(Reader.lerString("Informe o CPF do Diretor: "));
+        diretor.setSalario(Reader.lerDouble("Informe o salário do Diretor: R$ "));
+        diretor.setDependentes(Reader.lerInt("Informe a quantidade de dependentes do Diretor: "));
+
+        return diretor;
+    }
+
+    private static Professor cadastrarProfessor() {
+        Professor professor = new Professor();
+
+        professor.setNome(Reader.lerString("Informe o nome do Professor: "));
+        professor.setCpf(Reader.lerString("Informe o CPF do Professor: "));
+        professor.setSalario(Reader.lerDouble("Informe o salário do Professor: R$ "));
+        professor.setTitulacao(Reader.lerString("Informe a titulação do Professor: "));
+
+        return professor;
+    }
+
     public static void cadastrar(Universidade universidade) {
         boolean continuar = true;
 
@@ -11,27 +33,11 @@ public class View{
                     1, 2
             );
 
-            if (escolha == 1) {
-                Diretor diretor = new Diretor();
-
-                diretor.setNome(Reader.lerString("Informe o nome do Diretor: "));
-                diretor.setCpf(Reader.lerString("Informe o CPF do Diretor: "));
-                diretor.setSalario(Reader.lerDouble("Informe o salário do Diretor: R$ "));
-                diretor.setDependentes(Reader.lerInt("Informe a quantidade de dependentes do Diretor: "));
-
-                universidade.adicionarFuncionario(diretor);
-            } else {
-                Professor professor = new Professor();
-
-                professor.setNome(Reader.lerString("Informe o nome do Professor: "));
-                professor.setCpf(Reader.lerString("Informe o CPF do Professor: "));
-                professor.setSalario(Reader.lerDouble("Informe o salário do Professor: R$ "));
-                professor.setTitulacao(Reader.lerString("Informe a titulação do Professor: "));
-
-                universidade.adicionarFuncionario(professor);
-            }
+            Funcionario funcionario = (escolha == 1) ? cadastrarDiretor() : cadastrarProfessor();
+            universidade.adicionarFuncionario(funcionario);
 
             continuar = Reader.lerBoolean("Deseja continuar? [S]Sim [N]Não: ", "Digite somente [S]Sim ou [N]Não", "S", "N");
         }
     }
+
 }
