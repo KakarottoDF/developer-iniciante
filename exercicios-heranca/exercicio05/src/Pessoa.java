c8909d258a16e8bf85b35bbde7cc83676b6d808f
Cristiano Ayub <cristiano.ayub@gmail.com>
2025-04-21 15:53:55 -0300

    Exercicio05

--- exercicios-heranca/exercicio05/src/Pessoa.java
+++ exercicios-heranca/exercicio05/src/Pessoa.java
@@ -13,8 +13,13 @@ public class Pessoa extends Familia {
 
     @Override
     public boolean equals(Object obj) {
-        if (this == obj) return true;
-        if (obj == null || getClass() != obj.getClass()) return false;
+        if (this == obj){
+            return true;
+        }else{
+            if (obj == null || getClass() != obj.getClass()) {
+                return false;
+            }
+        }
         Pessoa pessoa = (Pessoa) obj;
         return getCodigoInteiro() == pessoa.getCodigoInteiro();
     }
