d36e3cd1a254e757b7b27aae6e1efa1ff1e17d83
Cristiano Ayub <cristiano.ayub@gmail.com>
2025-04-24 10:50:07 -0300

    Exercicio01

---
+++ exercicios-chatgpt/exercicio01/src/Classificao.java
@@ -0,0 +1,4 @@
+public enum Classificao{
+    FISICO,
+    DIGITAL;
+}
---
+++ exercicios-chatgpt/exercicio01/src/Material.java
@@ -0,0 +1,6 @@
+public class Material {
+    private String nome;
+    private boolean statusDisponibilidade;
+    private String informacaoAdicional;
+
+}
