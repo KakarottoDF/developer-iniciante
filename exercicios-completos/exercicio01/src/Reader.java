cb1954f517f7e13e616a59ca4e18313ed2e666be
Cristiano Ayub <cristiano.ayub@gmail.com>
2025-04-25 16:40:48 -0300

    Exercício01

--- exercicios-completos/exercicio01/src/Reader.java
+++ exercicios-completos/exercicio01/src/Reader.java
@@ -1,2 +1,85 @@
-public class Reader {
+import java.util.Scanner;
+
+public class Reader{
+    public static String lerString(){
+        return new Scanner(System.in).nextLine().toUpperCase();
+    }
+
+    public static String lerString(String msg){
+        System.out.println(msg);
+        return lerString();
+    }
+
+    public static String lerString(String msg, String msgError, String entrada, String saida){
+        String leitor;
+
+        do{
+            leitor = lerString(msg);
+            if(!leitor.equals(entrada) || !leitor.equals(saida)){
+                System.out.println(msgError);
+            }
+        }while(!leitor.equals(entrada) && !leitor.equals(saida));
+        return leitor;
+    }
+
+    public static boolean lerBoolean(){
+        return new Scanner(System.in).nextBoolean();
+    }
+
+    public static boolean lerBoolean(String msg){
+        System.out.println(msg);
+        return lerBoolean();
+    }
+
+    public static boolean lerBoolean(String msg, String msgError, String entrada, String saida){
+        String leitor;
+        boolean resultado = true;
+
+        do{
+            leitor = lerString(msg);
+            if(!leitor.equals(entrada) && !leitor.equals(saida)){
+                System.out.println(msgError);
+            }else{
+                if(leitor.equals(entrada)){
+                    resultado = true;
+                }else{
+                    if(leitor.equals(saida)){
+                        resultado = false;
+                    }
+                }
+            }
+        }while(!leitor.equals(entrada) && !leitor.equals(saida));
+        return resultado;
+    }
+
+    public static int lerInt(){
+        return new Scanner(System.in).nextInt();
+    }
+
+    public static int lerInt(String msg){
+        System.out.println(msg);
+        return lerInt();
+    }
+
+    public static int lerInt(String msg, String msgError, int min, int max){
+        int leitor;
+
+        do{
+            leitor = lerInt(msg);
+            if(leitor < min || leitor > max) {
+                System.out.println(msgError);
+            }
+        }while(leitor < min || leitor > max);
+        return leitor;
+    }
+
+    public static double lerDouble(){
+        return new Scanner(System.in).nextDouble();
+    }
+
+    public static double lerDouble(String msg){
+        System.out.println(msg);
+        return lerDouble();
+    }
+
 }
