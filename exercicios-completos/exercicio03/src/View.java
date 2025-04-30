f1fadaa65836fa00c8c38a42aaddc4bc0ca495ae
Cristiano Ayub <cristiano.ayub@gmail.com>
2025-04-30 16:35:26 -0300

    Exercício03

--- exercicios-completos/exercicio03/src/View.java
+++ exercicios-completos/exercicio03/src/View.java
@@ -1,2 +1,21 @@
-public class View {
+public class View{
+    public static Administrativo cadastrarAdministrativo(){
+        return new Administrativo(
+                Reader.lerString("Informe o nome do responsável: "),
+                Reader.lerString("Informe o número do telefone do responsável: "),
+                Reader.lerBoolean("Esse sistema é Full Time? [S]SIm [N]Não: ", "Digite somente [S]Sim ou [N]Não: ", "S", "N"),
+                Reader.lerString("Informe o nome desse sistema: "),
+                Reader.lerInt("Informe a quantidade de usuários simultâneos neste sistema: ")
+        );
+    }
+
+    public static void cadastrar(Sistema sistema){
+        boolean continuar = true;
+
+        while(continuar){
+            sistema.setAdministrativos(cadastrarAdministrativo());
+
+            continuar = Reader.lerBoolean("Deseja continuar com o cadastro de Sistema Administrativo? [S]Sim [N]Não: ", "Digite somente [S]Sim [N]Não", "S", "N");
+        }
+    }
 }
