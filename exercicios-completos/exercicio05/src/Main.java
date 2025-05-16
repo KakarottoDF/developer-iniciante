006f082760b9c6e4c6e8c1e5e58c2eb4a363f4dd
Cristiano Ayub <cristiano.ayub@gmail.com>
2025-05-16 16:12:30 -0300

    Exercício05

--- exercicios-completos/exercicio05/src/Main.java
+++ exercicios-completos/exercicio05/src/Main.java
@@ -2,18 +2,44 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+        boolean escolha;
+        int opcao;
         SistemaDeArquivos sistemaDeArquivos = new SistemaDeArquivos();
 
-        View.alimentarArrayDotNet(sistemaDeArquivos);
-        View.alimentarArrayC(sistemaDeArquivos);
+        do {
+            Printer.menu();
+            opcao = Reader.lerInt("ESCOLHA UMA DAS OPÇÕES: ", "DIGITE SOMENTE UM NÚMERO ENTRE 1 A 7", 1, 7);
 
+            switch(opcao) {
+                case 1:
+                    View.alimentarArrayDotNet(sistemaDeArquivos);
+                    break;
+                case 2:
+                    View.alimentarArrayC(sistemaDeArquivos);
+                    break;
+                case 3:
+                    Printer.imprimirArquivosOrdenadosPorTempoDeCompilacao(sistemaDeArquivos);
+                    break;
+                case 4:
+                    Printer.imprimirArquivoComMaiorTempoDeCompilacao(sistemaDeArquivos);
+                    break;
+                case 5:
+                    Printer.imprimirArquivosCondicionais(sistemaDeArquivos);
+                    break;
+                case 6:
+                    Printer.imprimirArquivosComTermo(sistemaDeArquivos);
+                    break;
+                case 7:
+                    escolha = true;
+                    continue;
+                default:
+                    System.out.println("ERRO INESPERADO.");
+                    break;
+            }
+
+            escolha = Reader.lerBoolean("DESEJA ENCERRAR O PROGRAMA? [S]SIM [N]NÃO: ", "ESCOLHA SOMENTE [S]SIM [N]NÃO: ", "S", "N");
+
+        } while (!escolha);
         //Printer.imprimirTodosOsSistemas(sistemaDeArquivos);
-
-        Printer.imprimirArquivosOrdenadosPorTempoDeCompilacao(sistemaDeArquivos);
-
-        //Printer.imprimirArquivoComMaiorTempoDeCompilacao(sistemaDeArquivos);
-
-        /*Printer.imprimirArquivosCondicionais(sistemaDeArquivos);
-        Printer.imprimirArquivosComTermo(sistemaDeArquivos);*/
     }
 }
--- exercicios-completos/exercicio05/src/Printer.java
+++ exercicios-completos/exercicio05/src/Printer.java
@@ -5,9 +5,9 @@ public class Printer {
     private static final int LINHA_FINAL = 7000;
     private static final String TERMO = "PROVA2";
 
-    public static void imprimirTodosOsSistemas(SistemaDeArquivos sistemaDeArquivos){
+    /*public static void imprimirTodosOsSistemas(SistemaDeArquivos sistemaDeArquivos){
         System.out.println(sistemaDeArquivos);
-    }
+    }*/
 
     //interface comparable
     //
@@ -30,7 +30,7 @@ public class Printer {
     }
 
     public static void imprimirArquivosCondicionais(SistemaDeArquivos sistema) {
-        System.out.println("\nIII. ARQUIVOS ENTRE " + LINHA_INICIAL + " E " + LINHA_FINAL + " LINHAS:");
+        System.out.println("\nARQUIVOS ENTRE " + LINHA_INICIAL + " E " + LINHA_FINAL + " LINHAS:");
 
         for (DotNet dotnet : sistema.getDotNets()) {
             if (dotnet.getQtdLinhas() >= LINHA_INICIAL && dotnet.getQtdLinhas() <= LINHA_FINAL) {
@@ -46,7 +46,7 @@ public class Printer {
     }
 
     public static void imprimirArquivosComTermo(SistemaDeArquivos sistema) {
-        System.out.println("\nIV. ARQUIVOS COM " + TERMO + " NO NOME:");
+        System.out.println("\nARQUIVOS COM " + TERMO + " NO NOME:");
 
         for (DotNet dotnet : sistema.getDotNets()) {
             if (dotnet.getNome().toUpperCase().contains(TERMO)) {
@@ -59,5 +59,15 @@ public class Printer {
                 System.out.println(c);
             }
         }
+    }
+
+    public static void menu(){
+        System.out.println("[1] CADASTRAR ARQUIVOS DOTNET");
+        System.out.println("[2] CADASTRAR ARQUIVOS C");
+        System.out.println("[3] LISTAR ARQUIVOS ORDENADOS EM FORMA DECRESCENTE POR TEMPO DE COMPILAÇÃO");
+        System.out.println("[4] LISTAR ARQUIVO QUE MAIS DEMORA PARA SER COMPILADO");
+        System.out.println("[5] LISTAR ARQUIVOS QUE POSSUEM ENTRE" + LINHA_INICIAL + " E " + LINHA_FINAL + " LINHAS.");
+        System.out.println("[6] LISTAR ARQUIVOS QUE POSSUEM O TERMO " + TERMO + " NO NOME.");
+        System.out.println("[7] SAIR");
     }
 }
