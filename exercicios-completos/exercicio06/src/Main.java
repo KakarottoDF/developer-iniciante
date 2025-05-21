a71cbd5aa712a137b0570127172ee9894590d3df
Cristiano Ayub <cristiano.ayub@gmail.com>
2025-05-21 12:59:50 -0300

    Exercício06

--- exercicios-completos/exercicio06/src/Main.java
+++ exercicios-completos/exercicio06/src/Main.java
@@ -1,10 +1,32 @@
 public class Main {
     public static void main(String[] args) {
+        boolean escolha;
+        int opcao;
         Governo governo = new Governo();
 
-        View.cadastrarTerroristaSuicida(governo);
+        do {
+            Printer.menu();
 
-        System.out.println(governo);
+            opcao = Reader.lerInt("Escolha uma das opções: ", "Escolha somente um número entre 1 a 4", 1, 4);
 
+            switch (opcao) {
+                case 1:
+                    View.cadastrarTerroristaSuicida(governo);
+                    break;
+                case 2:
+                    View.cadastrarTerroristaNaoSuicida(governo);
+                    break;
+                case 3:
+                    break;
+                case 4:
+                    break;
+                default:
+                    System.out.println("ERRO INESPERADO");
+                    break;
+            }
+
+            escolha = Reader.lerBoolean("DESEJA ENCERRAR O PROGRAMA? [S]SIM [N]NÃO: ", "ESCOLHA SOMENTE [S]SIM [N]NÃO: ", "S", "N");
+
+        }while(!escolha);
     }
 }
--- exercicios-completos/exercicio06/src/Printer.java
+++ exercicios-completos/exercicio06/src/Printer.java
@@ -1,2 +1,14 @@
 public class Printer {
+
+    public static void imprimirListaTodosTerroristas(Governo governo){
+        System.out.println(governo);
+    }
+
+    public static void menu(){
+        System.out.println("[1] CADASTRAR TERRORISTA SUICIDA");
+        System.out.println("[2] CADASTRAR TERRORISTA NÃO SUICIDA");
+        System.out.println("[3] LISTAR ARQUIVOS ORDENADOS EM FORMA DECRESCENTE POR TEMPO DE COMPILAÇÃO");
+        System.out.println("[4] LISTAR ARQUIVO QUE MAIS DEMORA PARA SER COMPILADO");
+        System.out.println("[7] SAIR");
+    }
 }
