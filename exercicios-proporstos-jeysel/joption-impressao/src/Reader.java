d63800dfc45ae07099beb2f0b8f9b507c47cb2c5
Cristiano Ayub <cristiano.ayub@gmail.com>
2025-05-26 15:09:09 -0300

    Estudando para Prova

--- exercicios-proporstos-jeysel/joption-impressao/src/Reader.java
+++ exercicios-proporstos-jeysel/joption-impressao/src/Reader.java
@@ -7,7 +7,23 @@ public class Reader {
     }
 
     public static int lerInt(String msg){
-        String input = JOptionPane.showInputDialog(null, msg);
-        return Integer.parseInt(input.trim());
+        String input = JOptionPane.showInputDialog(null, msg).trim();
+        return Integer.parseInt(input);
+    }
+
+    public static boolean lerBoolean(String msg, String msgError, String entrada, String saida) {
+        String leitor;
+        boolean resultado = true;
+
+        do {
+            leitor = JOptionPane.showInputDialog(null, msg).trim().toUpperCase();
+            if (!leitor.equals(entrada) && !leitor.equals(saida)) {
+                JOptionPane.showMessageDialog(null, msgError);
+            } else {
+                resultado = leitor.equals(entrada);
+            }
+        } while (!leitor.equals(entrada) && !leitor.equals(saida));
+
+        return resultado;
     }
 }
--- exercicios-proporstos-jeysel/joption-impressao/src/View.java
+++ exercicios-proporstos-jeysel/joption-impressao/src/View.java
@@ -17,7 +17,11 @@ public class View {
         );
     }
 
-    public static void cadastrarPessoa(GerenciarPessoas gerenciarPessoas){
-        gerenciarPessoas.adicionar(alimentarArrayPessoa());
+    public static void cadastrarPessoa(GerenciarPessoas gerenciarPessoas) {
+        boolean flag;
+        do {
+            gerenciarPessoas.adicionar(alimentarArrayPessoa());
+            flag = Reader.lerBoolean("DESEJA ENCERRAR O PROGRAMA? [S]SIM [N]NÃO", "DIGITE SOMENTE [S]SIM [N]NÃO", "S", "N");
+        }while (!flag);
     }
 }
