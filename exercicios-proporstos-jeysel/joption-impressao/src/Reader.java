481bcd0db889b5ac2737b1439c094b92bd0e580b
Cristiano Ayub <cristiano.ayub@gmail.com>
2025-05-26 14:42:53 -0300

    Estudando para Prova

--- exercicios-proporstos-jeysel/joption-impressao/src/Reader.java
+++ exercicios-proporstos-jeysel/joption-impressao/src/Reader.java
@@ -1,12 +1,13 @@
-import java.util.Scanner;
+import javax.swing.*;
 
 public class Reader {
-    public static String lerString(){
-        return new Scanner(System.in).nextLine().toUpperCase();
-    }
 
     public static String lerString(String msg){
-        System.out.println(msg);
-        return lerString();
+        return JOptionPane.showInputDialog(null, msg).trim().toUpperCase();
+    }
+
+    public static int lerInt(String msg){
+        String input = JOptionPane.showInputDialog(null, msg);
+        return Integer.parseInt(input.trim());
     }
 }
--- exercicios-proporstos-jeysel/joption-impressao/src/View.java
+++ exercicios-proporstos-jeysel/joption-impressao/src/View.java
@@ -1,5 +1,23 @@
 public class View {
+
+    public static Endereco alimentarEndereco() {
+        return new Endereco(
+                Reader.lerString("Informe o endereço: "),
+                Reader.lerString("Informe a cidade: "),
+                Reader.lerString("Informe o estado: ")
+        );
+    }
     public static Pessoa alimentarArrayPessoa(){
 
+        return new Pessoa(
+          Reader.lerString("Informe o nome da pessoa: "),
+          Reader.lerInt("Informe a matrícula: "),
+          alimentarEndereco(),
+          Reader.lerInt("Informe o telefone: ")
+        );
+    }
+
+    public static void cadastrarPessoa(GerenciarPessoas gerenciarPessoas){
+        gerenciarPessoas.adicionar(alimentarArrayPessoa());
     }
 }
