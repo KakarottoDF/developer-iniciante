f85bd1f85902453b7eb4ef99ce73dee52bb51b8b
Cristiano Ayub <cristiano.ayub@gmail.com>
2025-05-29 10:27:07 -0300

    Design Pattern Observer

--- exercicios-proporstos-jeysel/padrao.projeto.observer/src/CarroRoubado.java
+++ exercicios-proporstos-jeysel/padrao.projeto.observer/src/CarroRoubado.java
@@ -33,8 +33,8 @@ public class CarroRoubado extends Observable implements Carro{
     }
 
     public void mudaEstado(){
-        setChanged();
-        notifyObservers(getAcao());
+        setChanged(); //Mudou o estado
+        notifyObservers(getAcao()); //Notifica os observadores
     }
 
     public String getAcao() {
