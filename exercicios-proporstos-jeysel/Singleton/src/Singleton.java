16a22081b039ebcea8d47d89c9decf5152f4f59d
Cristiano Ayub <cristiano.ayub@gmail.com>
2025-05-30 15:32:33 -0300

    Design Pattern Singleton

--- exercicios-proporstos-jeysel/Singleton/src/Singleton.java
+++ exercicios-proporstos-jeysel/Singleton/src/Singleton.java
@@ -2,7 +2,7 @@ public class Singleton {
     private static Singleton uniqueInstance;
 
     private Singleton(){
-
+        //Construtor criado para não deixar criar a instância da classe, pois é privada
     }
 
     public static synchronized Singleton getInstance(){
