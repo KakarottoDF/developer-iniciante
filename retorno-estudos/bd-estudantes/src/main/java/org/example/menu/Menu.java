15313cbe6babfa7506b01816e9c711075556c6ca
Cristiano Ayub <cristiano.ayub@gmail.com>
2025-07-30 09:10:33 -0300

    Banco de Dados de Estudante

--- retorno-estudos/bd-estudantes/src/main/java/org/example/menu/Menu.java
+++ retorno-estudos/bd-estudantes/src/main/java/org/example/menu/Menu.java
@@ -16,7 +16,19 @@ public class Menu {
         };
 
         while (true) {
-            int opcao = Reader.lerInt("Menu:\n0 - Cadastrar\n1 - Atualizar\n2 - Deletar\n3 - Buscar CPF\n4 - Buscar Nome\n5 - Listar Todos\n6 - Sair");
+            int opcao = Reader.lerInt(
+                    """
+                            ESCOLHA UMA DAS OPÇÕES:
+                            
+                            [0] CADASTRAR ALUNO
+                            [1] ATUALIZAR ALUNO
+                            [2] DELETAR ALUNO
+                            [3] BUSCAR ALUNO POR CPF
+                            [4] BUSCAR ALUNO POR NOME
+                            [5] LISTAR TODOS OS ALUNOS
+                            [6] SAIR DO SISTEMA
+                            
+                            """, "DIGITE SOMENTE UM NÚMERO ENTRE 0 E 6", 0, 6);
             if (opcao >= 0 && opcao < comandos.length){
                 comandos[opcao].executar();
             }
--- retorno-estudos/bd-estudantes/src/main/java/org/example/model/Reader.java
+++ retorno-estudos/bd-estudantes/src/main/java/org/example/model/Reader.java
@@ -3,12 +3,35 @@ package org.example.model;
 import javax.swing.JOptionPane;
 
 public class Reader {
+
     public static String lerString(String msg) {
-        return JOptionPane.showInputDialog(null, msg).trim();
+        return JOptionPane.showInputDialog(null, msg).trim().toUpperCase();
     }
 
     public static int lerInt(String msg) {
-        return Integer.parseInt(JOptionPane.showInputDialog(null, msg).trim());
+        String input = JOptionPane.showInputDialog(null, msg);
+        return Integer.parseInt(input.trim());
+    }
+
+    public static int lerInt(String msg, String msgError, int min, int max) {
+        int leitor = 0;
+        boolean valido = false;
+
+        do {
+            try {
+                String input = JOptionPane.showInputDialog(null, msg);
+                leitor = Integer.parseInt(input.trim());
+                if (leitor < min || leitor > max) {
+                    JOptionPane.showMessageDialog(null, msgError);
+                } else {
+                    valido = true;
+                }
+            } catch (NumberFormatException e) {
+                JOptionPane.showMessageDialog(null, msgError);
+            }
+        } while (!valido);
+
+        return leitor;
     }
 
     public static void mostrarMensagem(String msg) {
